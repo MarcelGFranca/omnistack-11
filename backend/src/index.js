ec5e4e55c1b8d57833342b80bedcb47f6e667705
MarcelGF <marcelgarciafranca@gmail.com>
2020-03-24 12:33:27 -0300

    added gitgnore

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.send('Hello Word')
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
 
